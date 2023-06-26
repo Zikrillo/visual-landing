bf72bc1922f5613d75446b78d67d9f395a7ecbf7
zikrillo <zikrillo5018@gmail.com>
2023-06-26 06:53:37 +0500

    adaptive

--- src/js/header.js
+++ src/js/header.js
@@ -7,14 +7,68 @@ const services = document.getElementById("services");
 const about = document.getElementById("about");
 const news = document.getElementById("news");
 const partners = document.getElementById("partners");
-const navItem = document.querySelectorAll(".header__navigation-item");;
+const navItem = document.querySelectorAll(".header__navigation-item");
+const footer = document.querySelector(".footer");
+const burger = document.querySelector(".burger");
+
 
 function isElementVisible(element) {
     var rect = element.getBoundingClientRect();
-    return rect.top <= 0;
+    return rect.top <= 200;
 }
 
 document.onscroll = ()=>{
+
+    if(isElementVisible(production)){
+        headerRow.innerHTML = ` 
+        <li class="header__navigation-item"><a href="/">МАГАЗИН</a></li>
+        <li class="header__navigation-item"><a href="#about"> О КОМПАНИИ</a></li>
+        <li class="header__navigation-item active"><a href="#production"> ПРОДУКЦИЯ</a></li>
+        <li class="header__navigation-item"><a href="#services">УСЛУГИ</a></li>
+        <li class="header__navigation-item"><a href="#news">АКЦИИ И НОВОСТИ</a></li>
+        <li class="header__navigation-item"><a href="#footer">ОБРАТНАЯ СВЯЗЬ</a></li>`;
+    }
+    if(isElementVisible(services)){
+        headerRow.innerHTML = ` 
+        <li class="header__navigation-item"><a href="/">МАГАЗИН</a></li>
+        <li class="header__navigation-item"><a href="#about"> О КОМПАНИИ</a></li>
+        <li class="header__navigation-item"><a href="#production"> ПРОДУКЦИЯ</a></li>
+        <li class="header__navigation-item active"><a href="#services">УСЛУГИ</a></li>
+        <li class="header__navigation-item"><a href="#news">АКЦИИ И НОВОСТИ</a></li>
+        <li class="header__navigation-item"><a href="#footer">ОБРАТНАЯ СВЯЗЬ</a></li>`;
+    }
+    if(isElementVisible(about)){
+        headerRow.innerHTML = ` 
+        <li class="header__navigation-item"><a href="/">МАГАЗИН</a></li>
+        <li class="header__navigation-item active"><a href="#about"> О КОМПАНИИ</a></li>
+        <li class="header__navigation-item"><a href="#production"> ПРОДУКЦИЯ</a></li>
+        <li class="header__navigation-item "><a href="#services">УСЛУГИ</a></li>
+        <li class="header__navigation-item"><a href="#news">АКЦИИ И НОВОСТИ</a></li>
+        <li class="header__navigation-item"><a href="#footer">ОБРАТНАЯ СВЯЗЬ</a></li>`;
+    }
+    if(isElementVisible(news)){
+        headerRow.innerHTML = ` 
+        <li class="header__navigation-item"><a href="/">МАГАЗИН</a></li>
+        <li class="header__navigation-item "><a href="#about"> О КОМПАНИИ</a></li>
+        <li class="header__navigation-item"><a href="#production"> ПРОДУКЦИЯ</a></li>
+        <li class="header__navigation-item "><a href="#services">УСЛУГИ</a></li>
+        <li class="header__navigation-item active"><a href="#news">АКЦИИ И НОВОСТИ</a></li>
+        <li class="header__navigation-item"><a href="#footer">ОБРАТНАЯ СВЯЗЬ</a></li>`;
+    }
+    if(isElementVisible(footer)){
+        headerRow.innerHTML = ` 
+        <li class="header__navigation-item"><a href="/">МАГАЗИН</a></li>
+        <li class="header__navigation-item"><a href="#about"> О КОМПАНИИ</a></li>
+        <li class="header__navigation-item"><a href="#production"> ПРОДУКЦИЯ</a></li>
+        <li class="header__navigation-item"><a href="#services">УСЛУГИ</a></li>
+        <li class="header__navigation-item"><a href="#news">АКЦИИ И НОВОСТИ</a></li>
+        <li class="header__navigation-item active"><a href="#footer">ОБРАТНАЯ СВЯЗЬ</a></li>`;
+    }
+    // if(isElementVisible(production)){
+    //     navItem[2].classList.remove("remove");
+    // }
+
+
     if(header.getBoundingClientRect().y <= -90){
         header.classList.remove("wrapper");
         header.classList.add("fixed");
@@ -29,3 +83,7 @@ document.onscroll = ()=>{
     }
     
 }
+
+burger.addEventListener("click", ()=>{
+    document.querySelector("header").classList.toggle("active-header");
+})
